3ba7cef3d59c81ada6a7896b9a18abc332e526be
shubhamsathe123 <shubhamsathe66@gmail.com>
2021-01-19 08:58:45 +0530

    welcome to fundoo registration

---
+++ src/RegistrationPage/register.jsx
@@ -0,0 +1,17 @@
+import React  from 'react';
+// import ReactDOM, { render } from 'react-dom';
+
+
+export default class RegisterForm extends React.Component{
+render() {
+    return (
+        <div class="my_form">
+             <div>
+                 <h1>welcome to fundoo registration</h1>
+             </div>
+        </div>
+    ) ;   
+}
+
+}
+
